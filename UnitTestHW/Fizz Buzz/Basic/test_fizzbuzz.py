fdc7ff8286d6f9833edb9bfdbb0c70284d7f4eb9
ericshen19555 <ericshen19555@gmail.com>
2025-03-16 14:22:01 +0800

    feat: fb basic test fin.

--- UnitTestHW/Fizz Buzz/Basic/test_fizzbuzz.py
+++ UnitTestHW/Fizz Buzz/Basic/test_fizzbuzz.py
@@ -1,13 +1,13 @@
 from horrible_fizzbuzz import fizzBuzz
 
-def test_3():
+def test_3(self, solution):
     assert fizzBuzz(3) == "Fizz"
 
-def test_5():
-    ...
+def test_5(self, solution):
+    assert fizzBuzz(5) == "Buzz"
 
-def test_15():
-    ...
+def test_15(self, solution):
+    assert fizzBuzz(15) == "Fizz" + "Buzz"
 
-def test_2():
-    ...
+def test_2(self, solution):
+    assert fizzBuzz(2) == "2"
